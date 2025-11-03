9b3c5a6f260ae50070434edcecdcac42f8d77e4b
Jo0088 <quelllequeg7@outlook.com>
2025-11-03 09:20:00 -0800

    feat(deploy): create deployment automation

---
+++ deploy/01_deploy_VeiledOrder.ts
@@ -0,0 +1,20 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  const deployed = await deploy("VeiledOrder", {
+    from: deployer,
+    log: true,
+    // Avoid redeploy if same bytecode already deployed (prevents duplicate nonces)
+    skipIfAlreadyDeployed: true,
+  });
+
+  console.log(`VeiledOrder contract: `, deployed.address);
+};
+export default func;
+func.id = "deploy_veiledOrder"; // id required to prevent reexecution
+func.tags = ["VeiledOrder"];
+
